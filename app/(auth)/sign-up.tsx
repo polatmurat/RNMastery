a03525d1c95e115337fc677da2e026583c9d1457
polatmurat <bomch4nte@gmail.com>
2024-08-25 17:43:45 +0300

    sign-up screen and input fields.

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,7 +1,23 @@
-import { images } from '@/constants'
+import CustomButton from '@/components/CustomButton'
+import InputField from '@/components/InputField'
+import OAuth from '@/components/OAuth'
+import { icons, images } from '@/constants'
+import { Link } from 'expo-router'
+import { useState } from 'react'
 import { Text, ScrollView, View, Image } from 'react-native'
 
 const SignUp = () => {
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const onSignUpPress = () => {
+
+  }
+
   return (
     <ScrollView className='flex-1 bg-white'>
       <View className='flex-1 bg-white'>
@@ -9,6 +25,44 @@ const SignUp = () => {
           <Image source={images.signUpCar} className='z-0 w-full h-[250px]' />
           <Text className='text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5'>You haven't an account?</Text>
         </View>
+
+        <View className='p-5'>
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
+          />
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            secureTextEntry={true}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+
+          <CustomButton title='Sign Up' onPress={onSignUpPress} className='mt-6' />
+
+          <OAuth />
+
+          <Link href='/(auth)/sign-in' className='text-lg text-center text-general-200 mt-10'>
+            <Text>Already have an account? {" "}</Text>
+            <Text className='text-primary-500'>Sign In</Text>
+          </Link>
+
+        </View>
+
+        {/* Verification Modal */}
+
       </View>
     </ScrollView>
   )
